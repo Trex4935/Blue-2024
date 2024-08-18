aa1427161d63fe1ef82ceb3163e91f39d1b0f52b
slashdasher <yoofiae@gmail.com>
2024-08-18 04:21:59 -0400

    Removed residual code from template and yaml should 100% work now

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,8 +26,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   // private final boolean x;
-  private final Robot m_Robot = new Robot();
-  private final Drivetrain m_exampleSubsystem = new Drivetrain();
   private final CommandJoystick m_dancePad = new CommandJoystick(1);
 
   private final Drivetrain m_Drivetrain = new Drivetrain();
